4c7d3928cf0bfed6e223c376b94a9a3f5d0578e4
negbie <nupagadi@posteo.de>
2018-03-13 16:35:48 +0100

    Partition by one day

--- database/migrate/migrate.go
+++ database/migrate/migrate.go
@@ -25,8 +25,11 @@ func CreateDatabases(addr []string) error {
 		dbExec(db, "CREATE DATABASE IF NOT EXISTS "+config.Setting.DBData+` DEFAULT CHARACTER SET = 'utf8' DEFAULT COLLATE 'utf8_general_ci';`)
 		dbExec(db, "CREATE DATABASE IF NOT EXISTS "+config.Setting.DBConf+` DEFAULT CHARACTER SET = 'utf8' DEFAULT COLLATE 'utf8_general_ci';`)
 		dbExec(db, `CREATE USER IF NOT EXISTS 'homer_user'@'localhost' IDENTIFIED BY 'homer_password';`)
+		dbExec(db, `CREATE USER IF NOT EXISTS 'homer_user'@'192.168.0.0/255.255.0.0' IDENTIFIED BY 'homer_password';`)
 		dbExec(db, "GRANT ALL ON "+config.Setting.DBData+`.* TO 'homer_user'@'localhost';`)
 		dbExec(db, "GRANT ALL ON "+config.Setting.DBConf+`.* TO 'homer_user'@'localhost';`)
+		dbExec(db, "GRANT ALL ON "+config.Setting.DBData+`.* TO 'homer_user'@'192.168.0.0/255.255.0.0';`)
+		dbExec(db, "GRANT ALL ON "+config.Setting.DBConf+`.* TO 'homer_user'@'192.168.0.0/255.255.0.0';`)
 
 	} else if config.Setting.DBDriver == "postgres" {
 		db, err = dbr.Open(config.Setting.DBDriver, " host="+addr[0]+" port="+addr[1]+" user="+config.Setting.DBUser+" password="+config.Setting.DBPass+" sslmode=disable", nil)
@@ -49,31 +52,33 @@ func CreateDatabases(addr []string) error {
 	return nil
 }
 
-func CreateDataTables(addr []string, box packr.Box) error {
+func CreateDataTables(addr []string, box packr.Box) (err error) {
+	nextDays := time.Now().Format("20060102") + "," + time.Now().Format("2006-01-02")
 	if config.Setting.DBDriver == "mysql" {
-		dbExecFile(addr, config.Setting.DBData, box.String("mysql/tbldata.sql"), "20110111")
+		err = dbExecFile(addr, config.Setting.DBData, box.String("mysql/tbldata.sql"), "20110111,2011-01-11", nextDays)
 	} else if config.Setting.DBDriver == "postgres" {
-		dbExecFile(addr, config.Setting.DBData, box.String("pgsql/tbldata.sql"), "20110111")
-		dbExecFile(addr, config.Setting.DBData, box.String("pgsql/pardata.sql"), "20110111")
-		dbExecFile(addr, config.Setting.DBData, box.String("pgsql/altdata.sql"), "20110111")
-		dbExecFile(addr, config.Setting.DBData, box.String("pgsql/inddata.sql"), "20110111")
+		err = dbExecFile(addr, config.Setting.DBData, box.String("pgsql/tbldata.sql"), "20110111,2011-01-11", nextDays)
+		err = dbExecFile(addr, config.Setting.DBData, box.String("pgsql/pardata.sql"), "20110111,2011-01-11", nextDays)
+		err = dbExecFile(addr, config.Setting.DBData, box.String("pgsql/altdata.sql"), "20110111,2011-01-11", nextDays)
+		err = dbExecFile(addr, config.Setting.DBData, box.String("pgsql/inddata.sql"), "20110111,2011-01-11", nextDays)
 	}
-	return nil
+	return err
 }
 
-func CreateConfTables(addr []string, box packr.Box) error {
+func CreateConfTables(addr []string, box packr.Box) (err error) {
+	nextDays := time.Now().Format("20060102") + "," + time.Now().Format("2006-01-02")
 	if config.Setting.DBDriver == "mysql" {
-		dbExecFile(addr, config.Setting.DBConf, box.String("mysql/tblconf.sql"), "20110111")
-		dbExecFile(addr, config.Setting.DBConf, box.String("mysql/insconf.sql"), "20110111")
+		err = dbExecFile(addr, config.Setting.DBConf, box.String("mysql/tblconf.sql"), "20110111,2011-01-11", nextDays)
+		err = dbExecFile(addr, config.Setting.DBConf, box.String("mysql/insconf.sql"), "20110111,2011-01-11", nextDays)
 	} else if config.Setting.DBDriver == "postgres" {
-		dbExecFile(addr, config.Setting.DBConf, box.String("pgsql/tblconf.sql"), "20110111")
-		dbExecFile(addr, config.Setting.DBConf, box.String("pgsql/indconf.sql"), "20110111")
-		dbExecFile(addr, config.Setting.DBConf, box.String("pgsql/insconf.sql"), "20110111")
+		err = dbExecFile(addr, config.Setting.DBConf, box.String("pgsql/tblconf.sql"), "20110111,2011-01-11", nextDays)
+		err = dbExecFile(addr, config.Setting.DBConf, box.String("pgsql/indconf.sql"), "20110111,2011-01-11", nextDays)
+		err = dbExecFile(addr, config.Setting.DBConf, box.String("pgsql/insconf.sql"), "20110111,2011-01-11", nextDays)
 	}
-	return nil
+	return err
 }
 
-func dbExecFile(addr []string, table, file, pattern string) {
+func dbExecFile(addr []string, table, file, old, new string) error {
 	var err error
 	var db *dbr.Connection
 
@@ -90,18 +95,21 @@ func dbExecFile(addr []string, table, file, pattern string) {
 	}
 	defer db.Close()
 
-	dot, err := dotsql.LoadFromStringReplace(file, pattern, time.Now().Format("20060102"))
+	dot, err := dotsql.LoadFromStringReplace(file, old, new)
 	if err != nil {
-		logp.Info("%v", err)
+		logp.Err("%v", err)
+		return err
 	}
 
-	for k := range dot.QueryMap() {
+	for k, v := range dot.QueryMap() {
+		logp.Debug("rotate", "%s\n\n", v)
 		_, err = dot.Exec(db, k)
 		if err != nil {
 			logp.Info("%v", err)
 		}
 	}
 	db.Close()
+	return nil
 }
 
 func dbExec(db *dbr.Connection, query string) {
