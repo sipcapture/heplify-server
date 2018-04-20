8cfbab025ad42878204df8ba2428695999aa6a52
Yong <yongwc@m1.com.sg>
2018-04-20 15:17:44 +0800

    rotation done by heplify-server
    mysql partition need at least 1 partion created, therefore need additional input so that the right partition can be created (duration and step)
    - this means different table creation need to be split up from tbldata into tbldatalog, tbldatasip and tbldataqos
    
    Change starttime to endtime. Assume step is 1hour for the partition because
    - pnr0 should be 0000h - 0100h
    - pnr1 should be 0100h - 0200h
    
    Therefore need to use endtime not starttime

--- database/files/homer7/mysql/parsip.sql
+++ database/files/homer7/mysql/parsip.sql
@@ -1,5 +1,5 @@
 -- name: create-partition-hep_proto_1_call
-ALTER TABLE hep_proto_1_call_TableDate ADD PARTITION (PARTITION pPartitionName_pnr0000 VALUES LESS THAN ( UNIX_TIMESTAMP('StartTime') ));
+ALTER TABLE hep_proto_1_call_TableDate ADD PARTITION (PARTITION pPartitionName_pnr0000 VALUES LESS THAN ( UNIX_TIMESTAMP('EndTime') ));
 
 -- name: create-partition-hep_proto_1_register
-ALTER TABLE hep_proto_1_register_TableDate ADD PARTITION (PARTITION pPartitionName_pnr0000 VALUES LESS THAN ( UNIX_TIMESTAMP('StartTime') ));
+ALTER TABLE hep_proto_1_register_TableDate ADD PARTITION (PARTITION pPartitionName_pnr0000 VALUES LESS THAN ( UNIX_TIMESTAMP('EndTime') ));
