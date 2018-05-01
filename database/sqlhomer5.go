7d1bbff44b9ab8e71e57eece77bd05733854392d
Eugen Biegler <nupagadi@posteo.de>
2018-05-01 19:12:19 +0200

    more utc...

--- database/sqlhomer5.go
+++ database/sqlhomer5.go
@@ -435,19 +435,19 @@ func (s *SQLHomer5) bulkInsert(query string, rows []interface{}, values string) 
 	if config.Setting.DBDriver == "mysql" {
 		switch query {
 		case "call":
-			query = "INSERT INTO sip_capture_call_" + time.Now().Format("20060102") + values
+			query = "INSERT INTO sip_capture_call_" + time.Now().UTC().Format("20060102") + values
 		case "register":
-			query = "INSERT INTO sip_capture_registration_" + time.Now().Format("20060102") + values
+			query = "INSERT INTO sip_capture_registration_" + time.Now().UTC().Format("20060102") + values
 		case "rest":
-			query = "INSERT INTO sip_capture_rest_" + time.Now().Format("20060102") + values
+			query = "INSERT INTO sip_capture_rest_" + time.Now().UTC().Format("20060102") + values
 		case "rtcp":
-			query = "INSERT INTO rtcp_capture_all_" + time.Now().Format("20060102") + values
+			query = "INSERT INTO rtcp_capture_all_" + time.Now().UTC().Format("20060102") + values
 		case "report":
-			query = "INSERT INTO report_capture_all_" + time.Now().Format("20060102") + values
+			query = "INSERT INTO report_capture_all_" + time.Now().UTC().Format("20060102") + values
 		case "dns":
-			query = "INSERT INTO dns_capture_all_" + time.Now().Format("20060102") + values
+			query = "INSERT INTO dns_capture_all_" + time.Now().UTC().Format("20060102") + values
 		case "log":
-			query = "INSERT INTO logs_capture_all_" + time.Now().Format("20060102") + values
+			query = "INSERT INTO logs_capture_all_" + time.Now().UTC().Format("20060102") + values
 		}
 	} else if config.Setting.DBDriver == "postgres" {
 		switch query {
