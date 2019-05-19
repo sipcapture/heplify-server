3faa511a2a25c2e03e781e10da882aa40a68c88c
negbie <nupagadi@posteo.de>
2019-05-19 18:27:57 +0200

    Add rpc client for cgrates

--- cdr/cgrates.go
+++ cdr/cgrates.go
@@ -2,7 +2,9 @@ package cdr
 
 import (
 	"encoding/binary"
-	"fmt"
+	"net/rpc"
+	"net/rpc/jsonrpc"
+	"time"
 
 	"github.com/VictoriaMetrics/fastcache"
 	"github.com/negbie/logp"
@@ -11,24 +13,62 @@ import (
 )
 
 const (
-	invite    = "INVITE"
-	bye       = "BYE"
 	cacheSize = 20 * 1024 * 1024
 )
 
+var (
+	attributeS = true
+	ralS       = true
+	chargerS   = true
+	store      = true
+	export     = true
+	thresholdS = true
+	statS      = true
+)
+
 type CGR struct {
-	cache *fastcache.Cache
+	cache  *fastcache.Cache
+	client *rpc.Client
+}
+
+type ArgV1ProcessEvent struct {
+	CGREvent
+	AttributeS *bool // control AttributeS processing
+	RALs       *bool // control if we rate the event
+	ChargerS   *bool // control ChargerS processing
+	Store      *bool // control storing of the CDR
+	Export     *bool // control online exports for the CDR
+	ThresholdS *bool // control ThresholdS
+	StatS      *bool // control sending the CDR to StatS for aggregation
+	*ArgDispatcher
+}
+
+type CGREvent struct {
+	Tenant string
+	ID     string
+	Time   *time.Time // event time
+	Event  map[string]interface{}
+}
+
+type ArgDispatcher struct {
+	APIKey  *string
+	RouteID *string
 }
 
 func (c *CGR) setup() error {
+	var err error
+	c.client, err = jsonrpc.Dial("tcp", "localhost:2012")
+	if err != nil {
+		logp.Warn("%v", err)
+		return err
+	}
+
 	c.cache = fastcache.New(cacheSize)
+	logp.Info("Run CGRateS Output, server: %s\n", config.Setting.CGRAddr)
 	return nil
 }
 
 func (c *CGR) send(hCh chan *decoder.HEP) {
-
-	logp.Info("Run CGRateS Output, server: %s\n", config.Setting.CGRAddr)
-
 	for {
 		pkt, ok := <-hCh
 		if !ok {
@@ -36,7 +76,7 @@ func (c *CGR) send(hCh chan *decoder.HEP) {
 		}
 
 		k := []byte(pkt.CID)
-		if pkt.SIP.CseqMethod == invite {
+		if pkt.SIP.CseqMethod == "INVITE" {
 			if !c.cache.Has(k) {
 				tu := uint64(pkt.Timestamp.UnixNano())
 				tb := make([]byte, 8)
@@ -45,10 +85,44 @@ func (c *CGR) send(hCh chan *decoder.HEP) {
 			}
 		}
 
-		if pkt.SIP.CseqMethod == bye {
+		if pkt.SIP.CseqMethod == "BYE" {
 			if buf := c.cache.Get(nil, k); buf != nil {
-				d := uint64(pkt.Timestamp.UnixNano()) - binary.BigEndian.Uint64(buf)
-				fmt.Println(pkt.CID, d/1e9)
+				d := time.Duration(uint64(pkt.Timestamp.UnixNano()) - binary.BigEndian.Uint64(buf))
+				if d < 1e15 {
+					args := &ArgV1ProcessEvent{
+						AttributeS: &attributeS,
+						RALs:       &ralS,
+						ChargerS:   &chargerS,
+						// Store:      &store,
+						// Export:     &export,
+						ThresholdS: &thresholdS,
+						StatS:      &statS,
+						CGREvent: CGREvent{
+							Tenant: "cgrates.org",
+							Event: map[string]interface{}{
+								//"EventName":   "TEST_EVENT",
+								//"ToR":         "*voice",
+								"OriginID":    "123452",
+								"Account":     "1001",
+								"Subject":     "1001",
+								"Destination": "1003",
+								"Category":    "call",
+								"Tenant":      "cgrates.org",
+								"Source":      "192.168.1.1",
+								"RequestType": "*prepaid",
+								//"SetupTime":   pkt.Timestamp.Add(-1 * d),
+								"AnswerTime": pkt.Timestamp,
+								"Usage":      d,
+							},
+						},
+					}
+
+					var reply string
+					if err := c.client.Call("CDRsV1.ProcessEvent", args, &reply); err != nil {
+						logp.Warn("%v", err)
+					}
+					logp.Info(reply)
+				}
 				c.cache.Del(k)
 			}
 		}
