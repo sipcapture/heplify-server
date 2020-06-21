adea09ab1ce4487b08753b75df8077f8b4cd0db4
negbie <nupagadi@posteo.de>
2020-06-21 18:42:12 +0200

    Use own utf8 fix func

--- decoder/decoder.go
+++ decoder/decoder.go
@@ -152,13 +152,6 @@ func (h *HEP) parse(packet []byte) error {
 	return nil
 }
 
-var fixUTF8 = func(r rune) rune {
-	if r == utf8.RuneError || r == '\x00' {
-		return -1
-	}
-	return r
-}
-
 func (h *HEP) normPayload() {
 	if config.Setting.Dedup {
 		ts := uint64(h.Timestamp.UnixNano())
@@ -182,12 +175,8 @@ func (h *HEP) normPayload() {
 		binary.BigEndian.PutUint64(tb, ts)
 		dedupCache.Set(kh, tb)
 	}
-	if !utf8.ValidString(h.Payload) {
-		h.Payload = strings.Map(fixUTF8, h.Payload)
-	}
-	if config.Setting.DBDriver == "postgres" && strings.Index(h.Payload, "\x00") > -1 {
-		h.Payload = strings.Map(fixUTF8, h.Payload)
-	}
+
+	h.Payload = toUTF8(h.Payload, "")
 }
 
 func (h *HEP) EscapeFields(w io.Writer, tag string) (int, error) {
@@ -302,4 +291,56 @@ func stb(s string) []byte {
 		Cap:  sh.Len,
 	}
 	return *(*[]byte)(unsafe.Pointer(&bh))
+}
+
+func toUTF8(s, replacement string) string {
+	var b strings.Builder
+
+	for i, c := range s {
+		if c != utf8.RuneError && c != '\x00' {
+			continue
+		}
+
+		_, wid := utf8.DecodeRuneInString(s[i:])
+		if wid == 1 {
+			b.Grow(len(s) + len(replacement))
+			b.WriteString(s[:i])
+			s = s[i:]
+			break
+		}
+	}
+
+	// Fast path for unchanged input
+	if b.Cap() == 0 { // didn't call b.Grow above
+		return s
+	}
+
+	invalid := false // previous byte was from an invalid UTF-8 sequence
+	for i := 0; i < len(s); {
+		c := s[i]
+		if c == '\x00' {
+			i++
+			invalid = false
+			continue
+		} else if c < utf8.RuneSelf {
+			i++
+			invalid = false
+			b.WriteByte(c)
+			continue
+		}
+		_, wid := utf8.DecodeRuneInString(s[i:])
+		if wid == 1 {
+			i++
+			if !invalid {
+				invalid = true
+				b.WriteString(replacement)
+			}
+			continue
+		}
+		invalid = false
+		b.WriteString(s[i : i+wid])
+		i += wid
+	}
+
+	return b.String()
 }
